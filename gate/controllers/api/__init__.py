d88130c2524acc671cd49fc5f6851edb47ebfb7d
Blake Rouse <blake.rouse@mail.cs>
2013-09-12 11:07:14 -0400

    Fix invalid imports, causing test fail.

--- gate/controllers/api/__init__.py
+++ gate/controllers/api/__init__.py
@@ -1,6 +1,6 @@
-from gate.controllers.base import Controller
+from gate.controllers.api.base import Controller
 #from gate.controllers.obj import ObjectController
-from gate.controllers.case import CaseController
+from gate.controllers.api.case import CaseController
 #from gate.controllers.evidence import EvidenceController
 
 __all__ = [
